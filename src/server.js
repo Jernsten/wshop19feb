18d6febd22de411d5119afc90928865f91a3ab6e
kemal.demirtas <kemal.demirtas@beamonpeople.se>
2020-02-26 07:13:43 +0100

    build wo warnings

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 // Här ska vi definiera vår server! Men vi ska inte starta den
 // (alltså app.listen(port, ()=>{}), har vi inte i denna fil, utan i index.js)
 const express = require('express')
+const productItem = require('../model/product')
 const sassMiddleware = require('node-sass-middleware')
 const app = express()
-const port = 8080
-const productItem = require('../model/product')
+const isDevMode = process.env.NODE_ENV == 'development'
+const port = process.env.PORT || 8080
 
 const ROUTE = {
     root: '/',
@@ -20,17 +21,18 @@ const VIEW = {
     addProduct: 'add-product'
 }
 
-app.use(sassMiddleware({ // tell sassMiddleware where src file and dest directory is
-    src: 'sass',
-    dest: 'public',
-    // debug: true, // för att skriva ut data till konsollen
-    outputStyle: 'compressed'
-}))
-// define a static folder, 'public'
+// sass middleware only used in development mode
+if (isDevMode) {
+    app.use(sassMiddleware({
+        src: 'sass',
+        dest: 'public',
+        // debug: true, // för att skriva ut data till konsollen
+        outputStyle: 'compressed'
+    }))
+}
+
 app.use(express.static('public'))
-// 
 app.use(express.urlencoded({ extended: true }));
-// define what view engine to use, ejs in this case
 app.set('view engine', 'ejs')
 
 
