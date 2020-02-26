3fa09b6b30bbe2385f3d4711f5a6798e13ddf13c
kemal.demirtas <kemal.demirtas@beamonpeople.se>
2020-02-26 07:51:37 +0100

    realease ready again

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Härifrån startar vi upp vår webshop
 const mongoose = require('mongoose')
-
+const databaseURL = process.env.MONGO_ATLAS_URL
 const { app, port } = require('./src/server')
-const dbConfig = require('./config/config')
 
 
 // Kicka igång servern
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
-mongoose.connect(dbConfig.databaseURL, dbOptions).then(() => {
+
+mongoose.connect(databaseURL, dbOptions).then(() => {
     app.listen(port, () => console.log(`App listening on port ${port}!`))
 })
 
